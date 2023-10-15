5082e4f33a5af3211e9f78222a4dc56b115a5a0a
Pain-Excalibur <ffggh.fn2@gmail.com>
2023-10-16 01:52:58 +0500

    added output

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using dummyDB.templates.cs;
-using Newtonsoft.Json;
 using System;
 using System.Globalization;
 using System.Reflection;
@@ -13,7 +12,25 @@ namespace dummyDB
             List<Book> books = Parser.Book(@"../../../data/Books.csv");
             List<Student> students = Parser.Student(@"../../../data/Students.csv");
             List<Record> records = Parser.Record(@"../../../data/Records.csv");
-
+            foreach (Book book in books)
+            {
+                Console.Write("Название книги: " + book.Title);
+                foreach (Record record in records)
+                {
+                    if(record.Book==book.Id)
+                    {
+                        foreach (Student student in students) 
+                        { 
+                            if (student.Id==record.Student)
+                            {
+                                Console.Write(" | Имя студента: " + student.Name + " | Дата: " + record.BorrowDate.ToString());
+                                break;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
